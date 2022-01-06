5959ceabe29aec2fdc81a08ccf695fc876637c0d
Conrad Brown <52186092+ConradJBrown@users.noreply.github.com>
2022-01-06 11:26:20 -0330

    Started working on the class for the API
    Working on creating the class to hold all the information about the card being grabbed with the API. This will be used in the GUI to display the cards information and to save the card to an inventory later

--- API.py
+++ API.py
@@ -1,17 +1,30 @@
 import requests
 import json
+import pandas as pd
 
 
-parameters = {'name': 'Fluffal Bear'}
-response = requests.get('https://db.ygoprodeck.com/api/v7/cardinfo.php', params=parameters).json()
+class YuGiOhAPI:
+    #Grabbing the card asked for from the API
+    
+    def __init__(self, card):
+        parameters = {'name': card}
+        response = requests.get('https://db.ygoprodeck.com/api/v7/cardinfo.php', params=parameters)
+        data = response.json()
+        df = pd.DataFrame.from_dict(data['data'])
+        self.name = df['name']
+        self.desc = df['desc']
+        self.atk = df['atk']
+        self.defence = df['def']
+        self.ID = df['id']
+        self.type = df['type']
 
+    def get_name():
+        return self.name
+    def get_atk():
+        return self.atk
+    def get_def():
+        return self.defence
+    def get_desc():
+        return self.desc
 
-for obj in response:
-    name = obj['data']['name']
-    print(name)
-#def jprint(obj):
-    # create a formatted string of the Python Json object
- #   text = json.dumps(obj, sort_keys=True, indent=4)
-  #  print(text)
-
-#jprint(response.json())
+        
