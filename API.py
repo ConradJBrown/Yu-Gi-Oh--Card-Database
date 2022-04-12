dd98cf2c4e6497ea30d851a304de5955abbad3f0
Conrad Brown <52186092+ConradJBrown@users.noreply.github.com>
2022-04-12 16:25:37 -0230

    Added Add_Card() Method
    Add_Card() will now check if the inventory file already exists, if it does it will add the card you want to the file, if not it will create the file and add the card to it!

--- API.py
+++ API.py
@@ -8,7 +8,8 @@ import csv
 
 URL = 'https://db.ygoprodeck.com/api/v7/cardinfo.php'
 FILE_INVENTORY = 'YuGiOh_Inventory.csv'
-INVENTORY = pd.read_csv('Database.csv')
+PATH_TO_FILE = 'Inventory.csv'
+FILE_EXISTS = exists(PATH_TO_FILE)
 
 def CardInfo(card):
     parameters = {'name': card}
@@ -28,14 +29,26 @@ def main():
     print(card.desc.to_string(index=False))
 
     add_to_Inv = input('Would you like to add this card to inv? [y/n] ')
-    while (add_to_Inv != 'y') or (add_to_Inv != 'n'):
+    while (add_to_Inv != 'y') and (add_to_Inv != 'n'):
         print('Input invalid. Please enter [y/n]')
         add_to_Inv = input('Would you like to add this card to inv? [y/n] ')
     if add_to_Inv == 'y':
-        pd.concat([INVENOTRY, card], ignore_index=True)
-        display(INVENORY)
+        Add_Card(card)
     else:
         print('Card was not added')
+
+def Add_Card(card):
+    if FILE_EXISTS:
+       INVENTORY = pd.read_csv(PATH_TO_FILE)
+       card['Stock'] = 1
+       INVENTORY = pd.concat([INVENTORY, card])
+    else:
+        INVENTORY = card
+        INVENTORY['Stock'] = 1
+    
+    INVENTORY.to_csv(PATH_TO_FILE,index=False, header=True)
+    print(INVENTORY['name'])
+    return
 
 main()
 
